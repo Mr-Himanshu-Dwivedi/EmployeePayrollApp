86dd9738732440a204579aa0aac4b59d13a53bc7
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-15 11:51:45 +0530

    UC9 Name Validation

--- src/main/java/com/app/employeeapp/dto/EmployeeDTO.java
+++ src/main/java/com/app/employeeapp/dto/EmployeeDTO.java
@@ -1,9 +1,14 @@
 package com.app.employeeapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class EmployeeDTO {
+    @NotBlank(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long")
     private String name;
+
     private double salary;
 }
